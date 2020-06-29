8bdb4253c20cc6380283f6a5290dcff8c950d6c4
Ian.Zheng <ian.zheng840402@gmail.com>
2020-06-29 09:40:38 +0800

    版本一

--- index.js
+++ index.js
@@ -1,213 +1,156 @@
-// TODO 可以再更改，改成定義贏的 type ， 之後直接用一個 func 迴圈跑贏的結果
 const gridDom = document.querySelectorAll('.grid')
 const nowPlayer = document.querySelector('.now-user')
 const player_1 = '一號玩家'
 const player_2 = '二號玩家'
 
-// 版本二
-// const winnerRule = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-//   [1, 4, 7],
-//   [2, 5, 8],
-//   [3, 6, 9],
-//   [1, 5, 9],
-//   [3, 5, 7]
-// ]
+let grid_1 = 0
+let grid_2 = 0
+let grid_3 = 0
+let grid_4 = 0
+let grid_5 = 0
+let grid_6 = 0
+let grid_7 = 0
+let grid_8 = 0
+let grid_9 = 0
+let nowStatus = true
 
-// const player1Data = []
-// const player2Data = []
+nowPlayer.innerHTML = nowStatus ? player_1 : player_2
 
-// let turn = 0
+const checkedEvent = (dom) => {
+  dom.innerHTML = nowStatus ? 'O' : 'X'
+  nowStatus = !nowStatus
+  nowPlayer.innerHTML = nowStatus ? player_1 : player_2
+}
 
-// const winnerCheck = (playerData) => {
-//   let isWin = false
-//   winnerRule.forEach((ruleItem) => {
-//     const rule = ruleItem.sort().toString()
-//     const data = playerData
-//       .filter(playerItem => ruleItem.includes(playerItem))
-//       .sort()
-//       .toString()
-//     if (rule === data) {
-//       isWin = true;
-//     }
-//   })
-//   return isWin
-// }
+const topRowLine = () => {
+  if (grid_1 === grid_2 && grid_2 === grid_3) {
+    winnerMsg()
+  }
+}
+const middleRowLine = () => {
+  if (grid_4 === grid_5 && grid_5 === grid_6) {
+    winnerMsg()
+  }
+}
+const bottomRowLine = () => {
+  if (grid_7 === grid_8 && grid_8 === grid_9) {
+    winnerMsg()
+  }
+}
+const leftColLine = () => {
+  if (grid_1 === grid_4 && grid_4 === grid_7) {
+    winnerMsg()
+  }
+}
+const middleColLine = () => {
+  if (grid_2 === grid_5 && grid_5 === grid_8) {
+    winnerMsg()
+  }
+}
+const rightColLine = () => {
+  if (grid_3 === grid_6 && grid_6 === grid_9) {
+    winnerMsg()
+  }
+}
+const topLeft2BottomRightLine = () => {
+  if (grid_1 === grid_5 && grid_5 === grid_9) {
+    winnerMsg()
+  }
+}
+const topRight2BottomLeftLine = () => {
+  if (grid_3 === grid_5 && grid_5 === grid_7) {
+    winnerMsg()
+  }
+}
 
-// gridDom.forEach((dom) => {
-//   dom.addEventListener('click', (e) => {
-//     if (turn < 10) {
-//       const value = Number(e.target.dataset.value)
-//       let isWin = false
-//       turn ++
-//       turn % 2 === 1 ? player1Data.push(value) : player2Data.push(value)
-//       e.target.innerHTML = turn % 2 === 1 ? 'O' : 'X'
-//       isWin = turn % 2 === 1 ? winnerCheck(player1Data) : winnerCheck(player2Data)
-//       if (isWin) {
-//         alert(`${nowPlayer.innerHTML} 獲勝！`)
-//         turn = 10
-//       }
-//       nowPlayer.innerHTML = turn % 2 === 1 ? player_2 : player_1
-//       if (!isWin && turn === 9) {
-//         alert('遊戲結束！')
-//       }
-//     }
-    
-//   }, { once: true })
-// })
+const winnerMsg = () => {
+  const name = !nowStatus ? player_1 : player_2
+  alert(`${name} 獲勝！`)
+}
 
-// 版本一
+document.querySelectorAll('.grid')[0].addEventListener('click', (e) => {
+  if (grid_1 === 0) {
+    grid_1 = nowStatus ? 1 : 2
+    checkedEvent(e.target)
+    topRowLine();
+    leftColLine();
+    topLeft2BottomRightLine();
+  }
+})
 
-// let grid_1 = 0
-// let grid_2 = 0
-// let grid_3 = 0
-// let grid_4 = 0
-// let grid_5 = 0
-// let grid_6 = 0
-// let grid_7 = 0
-// let grid_8 = 0
-// let grid_9 = 0
-// let nowStatus = true
+document.querySelectorAll('.grid')[1].addEventListener('click', (e) => {
+  if (grid_2 === 0) {
+    grid_2 = nowStatus ? 1 : 2
+    checkedEvent(e.target)
+    topRowLine();
+    middleColLine();
+  }
+})
 
-// nowPlayer.innerHTML = nowStatus ? player_1 : player_2
+document.querySelectorAll('.grid')[2].addEventListener('click', (e) => {
+  if (grid_3 === 0) {
+    grid_3 = nowStatus ? 1 : 2
+    checkedEvent(e.target)
+    topRowLine();
+    rightColLine();
+    topRight2BottomLeftLine();
+  }
+})
 
-// const checkedEvent = (dom) => {
-//   dom.innerHTML = nowStatus ? 'O' : 'X'
-//   nowStatus = !nowStatus
-//   nowPlayer.innerHTML = nowStatus ? player_1 : player_2
-// }
+document.querySelectorAll('.grid')[3].addEventListener('click', (e) => {
+  if (grid_4 === 0) {
+    grid_4 = nowStatus ? 1 : 2
+    checkedEvent(e.target)
+    middleRowLine();
+    leftColLine();
+  }
+})
 
-// const topRowLine = () => {
-//   if (grid_1 === grid_2 && grid_2 === grid_3) {
-//     winnerMsg()
-//   }
-// }
-// const middleRowLine = () => {
-//   if (grid_4 === grid_5 && grid_5 === grid_6) {
-//     winnerMsg()
-//   }
-// }
-// const bottomRowLine = () => {
-//   if (grid_7 === grid_8 && grid_8 === grid_9) {
-//     winnerMsg()
-//   }
-// }
-// const leftColLine = () => {
-//   if (grid_1 === grid_4 && grid_4 === grid_7) {
-//     winnerMsg()
-//   }
-// }
-// const middleColLine = () => {
-//   if (grid_2 === grid_5 && grid_5 === grid_8) {
-//     winnerMsg()
-//   }
-// }
-// const rightColLine = () => {
-//   if (grid_3 === grid_6 && grid_6 === grid_9) {
-//     winnerMsg()
-//   }
-// }
-// const topLeft2BottomRightLine = () => {
-//   if (grid_1 === grid_5 && grid_5 === grid_9) {
-//     winnerMsg()
-//   }
-// }
-// const topRight2BottomLeftLine = () => {
-//   if (grid_3 === grid_5 && grid_5 === grid_7) {
-//     winnerMsg()
-//   }
-// }
+document.querySelectorAll('.grid')[4].addEventListener('click', (e) => {
+  if (grid_5 === 0) {
+    grid_5 = nowStatus ? 1 : 2
+    checkedEvent(e.target)
+    middleRowLine();
+    middleColLine();
+    topLeft2BottomRightLine();
+    topRight2BottomLeftLine();
+  }
+})
 
-// const winnerMsg = () => {
-//   const name = !nowStatus ? player_1 : player_2
-//   alert(`${name} 獲勝！`)
-// }
+document.querySelectorAll('.grid')[5].addEventListener('click', (e) => {
+  if (grid_6 === 0) {
+    grid_6 = nowStatus ? 1 : 2
+    checkedEvent(e.target)
+    middleRowLine();
+    rightColLine();
+  }
+})
 
-// document.querySelectorAll('.grid')[0].addEventListener('click', (e) => {
-//   if (grid_1 === 0) {
-//     grid_1 = nowStatus ? 1 : 2
-//     checkedEvent(e.target)
-//     topRowLine();
-//     leftColLine();
-//     topLeft2BottomRightLine();
-//   }
-// })
+document.querySelectorAll('.grid')[6].addEventListener('click', (e) => {
+  if (grid_7 === 0) {
+    grid_7 = nowStatus ? 1 : 2
+    checkedEvent(e.target)
+    bottomRowLine();
+    leftColLine();
+    topRight2BottomLeftLine();
+  }
+})
 
-// document.querySelectorAll('.grid')[1].addEventListener('click', (e) => {
-//   if (grid_2 === 0) {
-//     grid_2 = nowStatus ? 1 : 2
-//     checkedEvent(e.target)
-//     topRowLine();
-//     middleColLine();
-//   }
-// })
+document.querySelectorAll('.grid')[7].addEventListener('click', (e) => {
+  if (grid_8 === 0) {
+    grid_8 = nowStatus ? 1 : 2
+    checkedEvent(e.target)
+    bottomRowLine();
+    middleColLine();
+  }
+})
 
-// document.querySelectorAll('.grid')[2].addEventListener('click', (e) => {
-//   if (grid_3 === 0) {
-//     grid_3 = nowStatus ? 1 : 2
-//     checkedEvent(e.target)
-//     topRowLine();
-//     rightColLine();
-//     topRight2BottomLeftLine();
-//   }
-// })
-
-// document.querySelectorAll('.grid')[3].addEventListener('click', (e) => {
-//   if (grid_4 === 0) {
-//     grid_4 = nowStatus ? 1 : 2
-//     checkedEvent(e.target)
-//     middleRowLine();
-//     leftColLine();
-//   }
-// })
-
-// document.querySelectorAll('.grid')[4].addEventListener('click', (e) => {
-//   if (grid_5 === 0) {
-//     grid_5 = nowStatus ? 1 : 2
-//     checkedEvent(e.target)
-//     middleRowLine();
-//     middleColLine();
-//     topLeft2BottomRightLine();
-//     topRight2BottomLeftLine();
-//   }
-// })
-
-// document.querySelectorAll('.grid')[5].addEventListener('click', (e) => {
-//   if (grid_6 === 0) {
-//     grid_6 = nowStatus ? 1 : 2
-//     checkedEvent(e.target)
-//     middleRowLine();
-//     rightColLine();
-//   }
-// })
-
-// document.querySelectorAll('.grid')[6].addEventListener('click', (e) => {
-//   if (grid_7 === 0) {
-//     grid_7 = nowStatus ? 1 : 2
-//     checkedEvent(e.target)
-//     bottomRowLine();
-//     leftColLine();
-//     topRight2BottomLeftLine();
-//   }
-// })
-
-// document.querySelectorAll('.grid')[7].addEventListener('click', (e) => {
-//   if (grid_8 === 0) {
-//     grid_8 = nowStatus ? 1 : 2
-//     checkedEvent(e.target)
-//     bottomRowLine();
-//     middleColLine();
-//   }
-// })
-
-// document.querySelectorAll('.grid')[8].addEventListener('click', (e) => {
-//   if (grid_9 === 0) {
-//     grid_9 = nowStatus ? 1 : 2
-//     checkedEvent(e.target)
-//     bottomRowLine();
-//     rightColLine();
-//     topLeft2BottomRightLine();
-//   }
-// })
+document.querySelectorAll('.grid')[8].addEventListener('click', (e) => {
+  if (grid_9 === 0) {
+    grid_9 = nowStatus ? 1 : 2
+    checkedEvent(e.target)
+    bottomRowLine();
+    rightColLine();
+    topLeft2BottomRightLine();
+  }
+})
